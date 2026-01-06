749c7ffc69cec67cd35794de4ba38cd629b8362c
Preeti <preetiraj823@gmail.com>
2026-01-06 13:01:44 +0530

    Create GCD Calculator.py
     A simple program to calculate the Greatest Common Divisor (GCD) of two input numbers.

---
+++ GCD Calculator.py
@@ -0,0 +1,10 @@
+n,m=map(int,input("Enter 2 no (eg, 40 10)\n:").split())
+l=[]
+for x in range(2,max([n,m])):
+    if n%x==0 and m%x==0:
+        l.append(x)
+
+if len(l)>=1:
+    print(f"Greatest Common Divisor of {n} and {m} is {max(l)}")
+else:
+    print(f"Greatest Common Divisor of {n} and {m} is 1")
